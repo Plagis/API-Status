a7a4bd43d3107788af87cd6c280982dc61004240
Plagis <165980009+Plagis@users.noreply.github.com>
2025-07-21 00:04:22 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -1,15 +1,23 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 API_URL = "https://janice.e-351.com/api/rest/v2/pricer?market=2"
-API_KEY = "iCwfwg6pdQzQwxTQtuPZX2iKBJFuErr5"
+API_KEY = os.getenv("API_KEY")  # get the API key from .env
+
 headers = {
     "accept": "application/json",
     "X-ApiKey": API_KEY,
     "content-Type": "text/plain"
 }
+
 data = {"market": 2}
+
 try:
-    response = requests.post("https://janice.e-351.com/api/rest/v2/pricer?market=2", headers=headers, json=data)
+    response = requests.post(API_URL, headers=headers, json=data)
     response.raise_for_status()  # Raises an exception for 4xx and 5xx errors
 
     # Handle the response if successful
@@ -18,3 +26,4 @@ try:
 except requests.exceptions.RequestException as e:
     print(f"Error: {e}")
     print(f"Response content: {response.text}")  # Print the error response
+
